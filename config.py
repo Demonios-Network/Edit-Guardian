a51937896e3787f8c32100b902b9231cffc95ef9
Demonios Network <fowziamujtaba@gmail.com>
2024-11-16 20:57:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
-API_ID = 
+API_ID = "24074986"
 
-API_HASH = ""
+API_HASH = "f4f6272a85d0e50e39a24cb378be118d"
 
 # Telegram bot token
-TELEGRAM_TOKEN = ""
+TELEGRAM_TOKEN = "8031501707:AAFdyMLscx91CBtMaE2U_ejK8LXYYywkim0"
 
 # Your telegram user ID (owner)
-OWNER_ID = 6346273488
+OWNER_ID = 7306043964
 
 SUDO_ID = [5907205317, 1805959544, 1284920298, 5881613383, 6864672519, 5458968679]
 
-MONGO_URI = ""
+MONGO_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
 
 DB_NAME = "hasnainkk"
 
 LOGGER = True
 
-BOT_NAME = "Edit Guardian"
+BOT_NAME = "Edit Guard"
 
-SUPPORT_ID = ()
+SUPPORT_ID = "-1002287005224"
